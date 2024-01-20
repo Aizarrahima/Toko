c56bc69b28ec8ec1189dbee3536a0adc8d0d2d2e
Aizar Rahima <aizarrahima@Aizar-Rahima.local>
2024-01-20 16:47:27 +0700

    feat: edit user

--- app/models/pengguna_model.php
+++ app/models/pengguna_model.php
@@ -43,6 +43,34 @@ class pengguna_model
         return $this->db->rowCount();
     }
 
+    public function updateDataPengguna($data)
+    {
+        $query = "UPDATE Pengguna SET 
+        NamaPengguna = :NamaPengguna,
+        Password = :Password,
+        NamaDepan = :NamaDepan,
+        NamaBelakang = :NamaBelakang,
+        NoHP = :NoHP,
+        Alamat = :Alamat,
+        IdAkses = :IdAkses
+        WHERE IdPengguna = :IdPengguna";
+
+        $this->db->query($query);
+
+        $this->db->bind('NamaPengguna', $data['NamaPengguna']);
+        $this->db->bind('Password', $data['Password']);
+        $this->db->bind('NamaDepan', $data['NamaDepan']);
+        $this->db->bind('NamaBelakang', $data['NamaBelakang']);
+        $this->db->bind('NoHP', $data['NoHP']);
+        $this->db->bind('Alamat', $data['Alamat']);
+        $this->db->bind('IdAkses', $data['IdAkses']);
+        $this->db->bind('IdPengguna', $data['IdPengguna']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
     public function hapusDataPengguna($IdPengguna)
     {
         $query = "DELETE FROM Pengguna WHERE IdPengguna = :IdPengguna";
